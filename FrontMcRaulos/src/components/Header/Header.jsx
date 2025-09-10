5740f41520074d907f08912ea60b85184c2d3760
AleSchmidt33 <aleschmoschi@gmail.com>
2025-09-10 18:48:19 -0300

    first commit

---
+++ FrontMcRaulos/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header(){
+    //Aca uso hook y declaro elementos
+  return (
+    <header className="header">
+      <h1 className="header-title">Bienvenido a mi app</h1>
+    </header>
+  )
+}
+
+export default Header
